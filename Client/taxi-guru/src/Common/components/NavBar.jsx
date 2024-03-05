f0881cd04330bed25aeb1d71c07d384cf654528b
Shuvam Jaswal <shuvamjaswalshuvam@gmail.com>
2024-03-05 16:51:55 +0530

    Code refactoring and esconfig

--- Client/taxi-guru/src/Common/components/NavBar.jsx
+++ Client/taxi-guru/src/Common/components/NavBar.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import { useState } from 'react';
-import logo from '/images/common/logo.png';
+import React from "react";
+import { useState } from "react";
+import logo from "/images/common/logo.png";
 function NavBar() {
   const [showService, setshowService] = useState(false);
- 
-    return (
-      <div>
-        <nav className="text-white bg-black flex justify-between">
+
+  return (
+    <div>
+      <nav className="text-white bg-black flex justify-between">
         <img
-              className="inline"
-              src="https://thetaxiguru.com/wp-content/uploads/2024/01/Untitled-design.png"
-              alt="Logo"
-            />
+          className="inline"
+          src="https://thetaxiguru.com/wp-content/uploads/2024/01/Untitled-design.png"
+          alt="Logo"
+        />
         <div>
-          <ul className='flex justify-end items-center' >
-            <li className='my-6 px-8 py-1 text-2xl  hover:bg-[#fa8907] rounded-3xl'>HOME</li>
-            <li className='my-6 px-8 py-1 text-2xl hover:bg-[#fa8907] rounded-2xl'>ABOUT US</li>
-            <li className='my-6 px-8 py-1  text-2xl hover:bg-[#fa8907] rounded-2xl'>SERVICES</li>
-            <li className='my-6 mr-4 px-8 py-1 text-2xl hover:bg-[#fa8907] rounded-2xl'>CONTACT US</li>
+          <ul className="flex justify-end items-center">
+            <li className="my-6 px-8 py-1 text-2xl  hover:bg-[#fa8907] rounded-3xl">
+              HOME
+            </li>
+            <li className="my-6 px-8 py-1 text-2xl hover:bg-[#fa8907] rounded-2xl">
+              ABOUT US
+            </li>
+            <li className="my-6 px-8 py-1  text-2xl hover:bg-[#fa8907] rounded-2xl">
+              SERVICES
+            </li>
+            <li className="my-6 mr-4 px-8 py-1 text-2xl hover:bg-[#fa8907] rounded-2xl">
+              CONTACT US
+            </li>
           </ul>
         </div>
-        </nav>
-      </div>
-  )
+      </nav>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
