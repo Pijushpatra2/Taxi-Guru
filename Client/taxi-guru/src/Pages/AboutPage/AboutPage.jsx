b655a178ef22cc0da28357c006304b53979c80be
Pijush Patra <pijush.patra024@gmail.com>
2024-03-07 16:30:53 +0530

    services Page

--- Client/taxi-guru/src/Pages/AboutPage/AboutPage.jsx
+++ Client/taxi-guru/src/Pages/AboutPage/AboutPage.jsx
@@ -1,34 +1,50 @@
 import React from "react";
 
+import car from "/images/home/home_1.jpeg";
+
 function AboutPage() {
   return (
     <div>
       <div className="bg-gray-400 h-8">
-        <p className=" font-black relative  left-4 top-1">About US</p>
+        <p className=" font-black ml-2 ">About US</p>
       </div>
-      <b className="text-orange-500 text-3xl relative top-6 left-9">
-        Who We Are?
-      </b>
-      <div className=" h-62 w-96  relative top-9 left-7 font-sans font-semibold">
-        At “the taxi guru”, we rethink travel in Seunk Mullanpur, New
-        Chandigarh, with a promise to somewhere safe and secure, comfort, and
-        reasonableness. <br />
-        <br />
-        Our different fleet of sedans, SUVs, and hatchbacks guarantees
-        custom-made answers for each explorer. <br />
-        <br />
-        Book effortlessly through our easy to use application or a basic call,
-        and trust “the taxi guru” for a dependable, secure, and pleasant
-        excursion.
+      <b className="text-orange-500 text-3xl ml-9">Who We Are?</b>
+
+      <div className="h-auto w-[100%]  flex justify-center">
+        <div className=" h-62 w-[40%] bg-red-500 font-sans font-semibold">
+          At “the taxi guru”, we rethink travel in Seunk Mullanpur, New
+          Chandigarh, with a promise to somewhere safe and secure, comfort, and
+          reasonableness. <br />
+          <br />
+          Our different fleet of sedans, SUVs, and hatchbacks guarantees
+          custom-made answers for each explorer. <br />
+          <br />
+          Book effortlessly through our easy to use application or a basic call,
+          and trust “the taxi guru” for a dependable, secure, and pleasant
+          excursion.
+        </div>
+        <div className="w-[40%] bg-red-500">
+          <img src={car} className="h-[100%] w-[100%] " alt="" />
+        </div>
       </div>
-      <div>
+      <div className="h-auto w-[100%]  flex flex-row-reverse justify-center mt-4">
+        <div className=" h-62 w-[40%] bg-red-500 font-sans font-semibold">
+          At “the taxi guru”, we rethink travel in Seunk Mullanpur, New
+          Chandigarh, with a promise to somewhere safe and secure, comfort, and
+          reasonableness. <br />
+          <br />
+          Our different fleet of sedans, SUVs, and hatchbacks guarantees
+          custom-made answers for each explorer. <br />
+          <br />
+          Book effortlessly through our easy to use application or a basic call,
+          and trust “the taxi guru” for a dependable, secure, and pleasant
+          excursion.
+        </div>
+        <div className="w-[40%] bg-red-500">
+          <img src={car} className="h-[100%] w-[100%] " alt="" />
+        </div>
       </div>
-
-
-
-
-
-      
+      <div></div>
     </div>
   );
 }
--- Client/taxi-guru/src/Pages/ServicesPage/Service1.jsx
+++ Client/taxi-guru/src/Pages/ServicesPage/Service1.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Background from "/images/home/home_1.jpeg";
+import { Button } from "@mui/material";
 export default function Service1() {
   return (
     <div>
@@ -10,16 +11,22 @@ export default function Service1() {
       />
 
       <div className="mt-[-150px] flex justify-center items-center px-6">
-        <div className="h-[550px] border-2 border-black bg-white shadow-2xl rounded-[43px] flex justify-center items-center">
+        <div className="h-[550px] border-2 border-black bg-white shadow-2xl rounded-[30px] flex justify-center items-center">
           <div className="h-[90%] w-[150%] px-5">
-            <img src={Background} className="h-[100%]" alt="error" />
+            <img
+              src={Background}
+              className="h-[100%] w-screen object-cover"
+              alt="error"
+            />
           </div>
 
-          <div className="flex justify-end item-start w-[150%] m-[38px]">
+          <div className="flex justify-end item-start w-[150%] h-[100%]">
             <div className="">
-              <h1 className="text-center font-bold text-4xl m-[30px]">SEDAN</h1>
+              <h1 className="text-center font-bold text-4xl mt-[30px]">
+                SEDAN
+              </h1>
 
-              <p className="text-2xl px-5">
+              <p className="text-2xl mr-[70px] mt-[48px] px-5">
                 Enjoy the exemplification of solace and style with our Sedans.
                 Our cutting edge fleet guarantees a smooth ride, including
                 extensive insides, environment control, Wi-Fi network, and USB
@@ -27,6 +34,11 @@ export default function Service1() {
                 or city visits, our vehicles guarantee a dependable and rich
                 transportation experience.
               </p>
+              <div className="text-center">
+                <button className="bg-orange-400 text-black font-bold px-4 py-4 rounded-full text-xl">
+                  Read More
+                </button>
+              </div>
             </div>
           </div>
         </div>
