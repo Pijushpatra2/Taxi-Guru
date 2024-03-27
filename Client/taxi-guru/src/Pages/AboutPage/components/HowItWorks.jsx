de4ebb4fb30734b51d5f7310511b857bf09b4f07
Shuvam Jaswal <shuvamjaswalshuvam@gmail.com>
2024-03-27 14:15:55 +0530

    added icons

--- Client/taxi-guru/src/Pages/AboutPage/components/HowItWorks.jsx
+++ Client/taxi-guru/src/Pages/AboutPage/components/HowItWorks.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import image from "/images/home/home_taxi.png";
+import Customercare from "/images/home/CustomerCare.png";
+import home_taxi from "/images/home/Taxi.png";
+import home_taxi2 from "/images/home/taxi2.png";
 
 export default function HowItWorks() {
   return (
@@ -9,19 +10,19 @@ export default function HowItWorks() {
         <div className="flex justify-around py-4 pb-8 flex-col md:flex-row gap-y-8">
           <div className="flex flex-col items-center justify-center  gap-3 text-center   ">
             <div className="h-24 w-24 bg-white rounded-full flex items-center justify-center p-2">
-              <img src={image} alt="" />
+              <img src={Customercare} alt="" />
             </div>
             <div className="text-xl">Call</div>
           </div>
           <div className="flex flex-col items-center justify-center  gap-3">
             <div className="h-24 w-24 bg-white rounded-full flex items-center justify-center p-2">
-              <img src={image} alt="" />
+              <img src={home_taxi} alt="" />
             </div>
             <div className="text-xl">Choose a car</div>
           </div>
           <div className="flex flex-col items-center justify-center  gap-3">
             <div className="h-24 w-24 bg-white rounded-full flex items-center justify-center p-2">
-              <img src={image} alt="" />
+              <img src={home_taxi2} className="scale-110" alt="" />
             </div>
             <div className="text-xl">Enjoy the ride</div>
           </div>
